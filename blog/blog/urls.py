109fb7b7cf822d195d2068945634260cc1ea3022
Deivisson-dev <darsonsilva83@gmail.com>
2023-09-08 21:11:32 -0300

    feat: implementando parte do cadastro

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -22,4 +22,5 @@ from task import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
 ]
--- blog/task/views.py
+++ blog/task/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
 
 def home(request):
-    return render(request, 'menu.html')
+    return render(request, 'home.html')
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    else:
+        if request.POST['senha1'] == request.POST['senha2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['senha1'])
+                user.save()
+
+                login(request, user)
+                return redirect('task')
+            except:
+                return render(request, 'signup.html', {'form': UserCreationForm(), 'error': 'Usuário já existe'})
+        return render(request, 'signup.html', {'form': UserCreationForm(), 'error': 'Senhas não conferem'})
